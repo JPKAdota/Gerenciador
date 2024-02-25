0d68e9a659b8656f8edaee49bc7e5f6876b87edc
JPKAdota <joaokadota@gmail.com>
2024-02-25 18:22:43 -0300

    Criando estilos

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from tkinter import *
 from tkinter import Tk, ttk
+#importando pillow
+from PIL import ImageTk,Image
+#importando barra de progresso
+from tkinter.ttk import Progressbar
+
+#importando Matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+
 
 #cores
 co0 = "#2e2d2b"
@@ -35,5 +46,38 @@ frameMeio.grid(row=1, column=0, padx=0, pady=1, sticky=NSEW)
 frameBaixo = Frame(janela, width=1043, height=300, bg=co1, relief="flat")
 frameBaixo.grid(row=2, column=0, padx=10, pady=0, sticky=NSEW)
 
+#Trabalhando no Frame Cima
+#acessando a imagem
+app_img = Image.open('logo.png')
+app_img = app_img.resize((45,45)) #dimensões da imagem
+app_img = ImageTk.PhotoImage(app_img)
 
+app_logo = Label(frameCima, image=app_img, text=" Orçamento pessoal", width=900, compound=LEFT, padx=5, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
+app_logo.place(x=0, y=0)
+
+
+#Percentagem----------------------------------------------
+
+def percentagem():
+    l_nome = Label(frameMeio, text="Porcentagem da Receita Gasta", height=1, anchor=NW, font=('Verdana 12'), bg= co1, fg=co4)
+    l_nome.place(x=7,y=5)
+
+    style= ttk.Style()
+    style.theme_use('default')
+    style.configure("black.Horizontal.TProgressbar", background='#daed6b')
+    style.configure("TProgressbar", thickness=25)
+
+    bar = Progressbar(frameMeio, length=180)
+    bar.place(x=10,y=35)
+    bar['value'] = 50
+
+    valor = 50
+
+    l_percetagem = Label(frameMeio, text="{:,.2f}%".format(valor), anchor=NW, font=('Verdana 12'), bg= co1, fg=co4)
+    l_percetagem.place(x=200,y=35)
+
+
+
+
+percentagem()
 janela.mainloop()
